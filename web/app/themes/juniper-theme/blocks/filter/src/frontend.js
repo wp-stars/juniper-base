0031b2706294c2e415063894e862b9f5faefcbfc
Will Nahmens <will@wp-stars.com>
2023-10-05 10:33:24 +0200

    update font references

--- web/app/themes/juniper-theme/blocks/filter/src/frontend.js
+++ web/app/themes/juniper-theme/blocks/filter/src/frontend.js
@@ -7,7 +7,6 @@ const filterDivs = document.querySelectorAll(".filter-update-me")
 
 filterDivs.forEach(div => {
     let data = JSON.parse(div.dataset.initialData)
-    console.log('filter data', data.posts)
     const root = ReactDOM.createRoot(div)
     root.render(<Filter {...data} />)
     div.classList.remove("filter-update-me")
