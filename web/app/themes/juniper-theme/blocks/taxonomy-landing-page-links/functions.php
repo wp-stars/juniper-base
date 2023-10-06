3cb078599a5333d628ce61b2b46e36f5e46012ae
Will Nahmens <will@wp-stars.com>
2023-10-06 13:05:57 +0200

    add taxonomy landing page links block

---
+++ web/app/themes/juniper-theme/blocks/taxonomy-landing-page-links/functions.php
@@ -0,0 +1,31 @@
+<?php
+
+add_action(
+    'wp_enqueue_scripts', function () {
+        if (has_block('acf/taxonomy-landing-page-links')) {
+            $time = time();
+            $theme_path = get_template_directory_uri();
+
+            wp_enqueue_style('taxonomy-landing-page-links-css', $theme_path . '/blocks/taxonomy-landing-page-links/style.css', array(), $time, 'all');
+            wp_enqueue_script('taxonomy-landing-page-links-js', $theme_path . '/blocks/taxonomy-landing-page-links/script.js', array(), $time, true);
+        }
+    }
+);
+
+add_filter(
+    'timber/acf-gutenberg-blocks-data/taxonomy-landing-page-links',
+    function ( $context ) {
+        global $post;
+
+        $terms = get_the_terms($post->ID, $context['fields']['taxonomy']);
+
+        foreach ($terms as $term) {
+            $term->fields = get_fields($term);
+            $term->link = get_term_link($term);
+        }
+
+        $context['terms'] = $terms;
+
+        return $context;
+    }
+);
