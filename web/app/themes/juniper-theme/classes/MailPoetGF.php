7f1576a4d2394bc099827329baaa96c64fc31523
Will Nahmens <willnahmens@gmail.com>
2023-11-20 09:43:43 +0100

    update mailpoet gf connection

--- web/app/themes/juniper-theme/classes/MailPoetGF.php
+++ web/app/themes/juniper-theme/classes/MailPoetGF.php
@@ -26,18 +26,14 @@ class MailPoetGF
         if($form['title'] != "Newsletter") return;
 
         $gfbody = array(                        
-            'name' => rgar( $entry, '1' ),
-            'email' => rgar( $entry, '3' ),
-            'list_id' => rgar( $entry, '5' ),        
+            'name' => rgar( $entry, '4' ),
+            'email' => rgar( $entry, '1' ),
+            'list_id' => rgar( $entry, '7' ),
         );
-    
+
         if (class_exists(\MailPoet\API\API::class)) {
             // Get MailPoet API instance
             $mailpoet_api = \MailPoet\API\API::MP('v1');
-            // Get available list so that a subscriber can choose in which to subscribe
-            $lists = $mailpoet_api->getLists();
-
-            
             $list_ids = array($gfbody["list_id"]);
 
             $subscriber = [];
@@ -47,31 +43,16 @@ class MailPoetGF
 
 
             // Check if subscriber exists. If subscriber doesn't exist an exception is thrown
-            $get_subscriber = false;
-            try {
-                $get_subscriber = $mailpoet_api->getSubscriber($subscriber['email']);
+            $get_subscriber = $mailpoet_api->getSubscriber($subscriber['email']);
+
+            if($get_subscriber) {
                 // In case subscriber exists just add them to new lists
                 $mailpoet_api->subscribeToLists($subscriber['email'], $list_ids);
-
-            } catch (\Exception $e) {
+            } else {
                 // Subscriber doesn't exist let's create one
                 $mailpoet_api->addSubscriber($subscriber, $list_ids);
                 $mailpoet_api->subscribeToLists($subscriber['email'], $list_ids);
             }
-
-            // try {
-            //     if (!$get_subscriber) {
-            //         // Subscriber doesn't exist let's create one
-            //         $mailpoet_api->addSubscriber($subscriber, $list_ids);
-            //         $mailpoet_api->subscribeToLists($subscriber['email'], $list_ids);
-            //     } else {
-            //         // In case subscriber exists just add them to new lists
-            //         $mailpoet_api->subscribeToLists($subscriber['email'], $list_ids);
-            //     }
-            // } catch (\Exception $e) {
-            //     $error_message = $e->getMessage();
-            //     die($error_message);
-            // }
         }   
     }
 }
