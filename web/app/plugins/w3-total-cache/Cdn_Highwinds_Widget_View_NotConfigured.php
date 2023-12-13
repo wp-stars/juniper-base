f3393db02f2101e44fc843543acb6d5cc8729d32
Will Nahmens <willnahmens@gmail.com>
2023-12-13 12:23:26 +0100

    update 404 text

---
+++ web/app/plugins/w3-total-cache/Cdn_Highwinds_Widget_View_NotConfigured.php
@@ -0,0 +1,11 @@
+<?php
+namespace W3TC;
+
+if (!defined('W3TC') ) {
+    die();
+}
+
+?>
+<div class="wrapper">
+    Not configured
+</div>
---
+++ web/app/plugins/w3-total-cache/Minify_GeneralPage_View_ShowHelpForce.js
@@ -0,0 +1,5 @@
+jQuery(
+    function () {
+        w3tc_show_minify_help();
+    }
+);
