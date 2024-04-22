f30d74e8274e1dae639aa25b0e27ecfd6f7a1bb9
Michael Ritsch <office@michael-ritsch.com>
2024-04-22 14:25:27 +0200

    fix: add Product to Musterbestellungswidget

--- web/app/themes/juniper-theme/assets/js/custom-musterbestellung.js
+++ web/app/themes/juniper-theme/assets/js/custom-musterbestellung.js
@@ -133,3 +133,24 @@ setInterval(() => {
     }
 }, 1000); // Poll every 500ms
 
+
+// add to musterbestellung
+function attachAppendEventListeners() {
+
+    let products = customMusterbestellungParams.musterbestellungProducts;
+    let productIds = products.map(product => product.id)
+
+    const appendButtons = document.querySelectorAll('.add-to-musterbestellung');
+    appendButtons.forEach(button => {
+        button.addEventListener('click', (e) => {
+            const productId = e.currentTarget.getAttribute('data-product-id');
+            // const updatedProductIds = productIds.filter(id => id !== productId);
+
+            productIds.push(productId);
+            setCookie(cookieName, JSON.stringify(productIds));
+            fetchAndUpdateMusterbestellung(true)
+        });
+    });
+}
+
+attachAppendEventListeners();
