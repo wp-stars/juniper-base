2d23b868e02f2fb0da1d0cf795071d7f5a81f0f5
Valentin Paul <Valentin@MacBook-Air.local>
2024-04-24 10:41:42 +0200

    navigation mobile

--- web/app/themes/juniper-theme/src/js/nav.js
+++ web/app/themes/juniper-theme/src/js/nav.js
@@ -2,3 +2,21 @@ const menuToggler = document.getElementById('navbarToggler');
 menuToggler.addEventListener('click', (e) => {
 	e.currentTarget.classList.toggle('navbar-open')
 })
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    var dropdownToggles = document.querySelectorAll('.dropdown-toggle');
+
+    dropdownToggles.forEach(function (toggle) {
+        toggle.addEventListener('click', function (event) {
+			event.preventDefault(); 
+            var expanded = this.getAttribute('aria-expanded') === 'true';
+            this.setAttribute('aria-expanded', !expanded);
+
+            var menu = this.closest('li').querySelector('.dropdown-menu');
+            if (menu) {
+                menu.style.display = expanded ? 'none' : 'block';
+            }
+        });
+    });
+});
