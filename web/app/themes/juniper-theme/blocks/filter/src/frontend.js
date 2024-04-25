0f2c12a9f4015e84dcf0dc08606c1488f6388c25
Valentin Paul <Valentin@MacBook-Air.local>
2024-04-25 16:41:34 +0200

    undo changes

--- web/app/themes/juniper-theme/blocks/filter/src/frontend.js
+++ web/app/themes/juniper-theme/blocks/filter/src/frontend.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from 'react-dom';
 import Filter from "./components/Filter";
-import FilterShop from "./components/FilterShop"; // Import the new component
+// import FilterShop from "./components/FilterShop"; // Import the new component
 
 const setupFilters = () => {
     // Handle Filter entries
@@ -13,14 +13,14 @@ const setupFilters = () => {
         div.classList.remove("filter-entry");
     });
 
-    // Handle FilterShop entries
-    const filterShopDivs = document.querySelectorAll(".filter-entry-shop");
-    filterShopDivs.forEach(div => {
-        let data = JSON.parse(div.dataset.initialData);
-        const root = ReactDOM.createRoot(div);
-        root.render(<FilterShop {...data} />);
-        div.classList.remove("filter-entry-shop");
-    });
+    // // Handle FilterShop entries
+    // const filterShopDivs = document.querySelectorAll(".filter-entry-shop");
+    // filterShopDivs.forEach(div => {
+    //     let data = JSON.parse(div.dataset.initialData);
+    //     const root = ReactDOM.createRoot(div);
+    //     root.render(<FilterShop {...data} />);
+    //     div.classList.remove("filter-entry-shop");
+    // });
 };
 
 document.addEventListener('DOMContentLoaded', setupFilters);
