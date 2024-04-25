8997822e0d440cae8154d8282ebd39c39ca53600
Valentin Paul <Valentin@MacBook-Air.local>
2024-04-25 11:33:52 +0200

    slick slider changes, no dots and product image

--- web/app/themes/juniper-theme/woocommerce/single-product/product-image.php
+++ web/app/themes/juniper-theme/woocommerce/single-product/product-image.php
@@ -1,30 +1,11 @@
 <?php
-/**
- * Single Product Image
- *
- * This template can be overridden by copying it to yourtheme/woocommerce/single-product/product-image.php.
- *
- * HOWEVER, on occasion WooCommerce will need to update template files and you
- * (the theme developer) will need to copy the new files to your theme to
- * maintain compatibility. We try to do this as little as possible, but it does
- * happen. When this occurs the version of the template file will be bumped and
- * the readme will list any important changes.
- *
- * @see     https://woo.com/document/template-structure/
- * @package WooCommerce\Templates
- * @version 7.8.0
- */
-
 defined( 'ABSPATH' ) || exit;
 
-// Note: `wc_get_gallery_image_html` was added in WC 3.3.2 and did not exist prior. This check protects against theme overrides being used on older versions of WC.
 if ( ! function_exists( 'wc_get_gallery_image_html' ) ) {
     return;
 }
 
 global $product;
-// var_dump($product);
-// die();
 
 $columns           = apply_filters( 'woocommerce_product_thumbnails_columns', 4 );
 $post_thumbnail_id = $product->get_image_id();
@@ -32,28 +13,36 @@ $wrapper_classes   = apply_filters(
     'woocommerce_single_product_image_gallery_classes',
     array(
         'woocommerce-product-gallery',
-        'woocommerce-product-gallery--' . ( $post_thumbnail_id ? 'with-images' : 'without-images' ),
-        'woocommerce-product-gallery--columns-' . absint( $columns ),
+        'woocommerce-product-gallery--' . ($post_thumbnail_id ? 'with-images' : 'without-images'),
+        'woocommerce-product-gallery--columns-' . absint($columns),
         'images',
         'max-h-[27.5rem]'
     )
 );
 ?>
-<div class="<?php echo esc_attr( implode( ' ', array_map( 'sanitize_html_class', $wrapper_classes ) ) ); ?>" data-columns="<?php echo esc_attr( $columns ); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
+<div class="<?php echo esc_attr( implode(' ', array_map('sanitize_html_class', $wrapper_classes)) ); ?>" data-columns="<?php echo esc_attr($columns); ?>" style="opacity: 0; transition: opacity .25s ease-in-out;">
     <div class="woocommerce-product-gallery__wrapper slick-slider flex justify-center items-center">
         <?php
-        if ( $post_thumbnail_id ) {
-            $attachment_ids = $product->get_gallery_image_ids();
-            foreach ( $attachment_ids as $attachment_id ) {
-				add_image_size( 'custom-size', 800, 800, true ); 
+        // Display the main product image as the first slide
+        if ($post_thumbnail_id) {
+            echo '<div>' . wp_get_attachment_image($post_thumbnail_id, 'custom-size', false, array('class' => 'max-h-[27.5rem] object-cover')) . '</div>';
+        }
+
+        // Display gallery images
+        $attachment_ids = $product->get_gallery_image_ids();
+        foreach ($attachment_ids as $attachment_id) {
+            if ($attachment_id !== $post_thumbnail_id) { // Ensure the main image is not repeated
+                add_image_size('custom-size', 800, 800, true);
                 echo '<div>' . wp_get_attachment_image($attachment_id, 'custom-size', false, array('class' => 'max-h-[27.5rem] object-cover')) . '</div>';
             }
-        } else {
+        }
+
+        // Fallback for no images
+        if (!$post_thumbnail_id && empty($attachment_ids)) {
             echo '<div class="woocommerce-product-gallery__image--placeholder">';
-            echo sprintf( '<img src="%s" alt="%s" class="wp-post-image" />', esc_url( wc_placeholder_img_src( 'woocommerce_single' ) ), esc_html__( 'Awaiting product image', 'woocommerce' ) );
+            echo sprintf('<img src="%s" alt="%s" class="wp-post-image" />', esc_url(wc_placeholder_img_src('woocommerce_single')), esc_html__('Awaiting product image', 'woocommerce'));
             echo '</div>';
         }
         ?>
     </div>
 </div>
-
