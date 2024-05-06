24c1873e1526142937b5bf7cc2d375100c3bc25e
Valentin Paul <Valentin@MacBook-Air.local>
2024-05-06 11:10:39 +0200

    label for FilterShop purchasability

--- web/app/themes/juniper-theme/blocks/filter/build/frontend.asset.php
+++ web/app/themes/juniper-theme/blocks/filter/build/frontend.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '5668616dc9742ae409cf');
+<?php return array('dependencies' => array('react', 'react-dom'), 'version' => '754a11c738cac4a4a722');
