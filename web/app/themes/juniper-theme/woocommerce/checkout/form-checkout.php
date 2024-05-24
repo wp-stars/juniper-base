1b1adbadd8d3d9ee86568652777e59123bf65ec4
Michael Ritsch <office@michael-ritsch.com>
2024-05-24 15:44:19 +0200

    fix: update font size in checkout

--- web/app/themes/juniper-theme/woocommerce/checkout/form-checkout.php
+++ web/app/themes/juniper-theme/woocommerce/checkout/form-checkout.php
@@ -51,7 +51,7 @@ if ( ! $checkout->is_registration_enabled() && $checkout->is_registration_requir
 	
 	<?php do_action( 'woocommerce_checkout_before_order_review_heading' ); ?>
 	
-	<h3 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
+	<h2 id="order_review_heading"><?php esc_html_e( 'Your order', 'woocommerce' ); ?></h3>
 	
 	<?php do_action( 'woocommerce_checkout_before_order_review' ); ?>
 
