28d038566c0cc5cd58146021373fdc5802cad202
Michael Ritsch <office@michael-ritsch.com>
2024-05-29 16:35:03 +0200

    fix: add language to transition cache token

--- web/app/themes/juniper-theme/blocks/filter/functions.php
+++ web/app/themes/juniper-theme/blocks/filter/functions.php
@@ -163,7 +163,8 @@ function wps_get_filter_posts( $post_type, $taxonomies, $page, $search = '' ) {
 		$args['tax_query'] = $tax_query;
 	}
 
-	$cachingToken  = 'wps_filter_cache_' . md5( json_encode( $args ) );
+    $currentLang = apply_filters( 'wpml_current_language', NULL );
+    $cachingToken  = 'wps_filter_cache_' . $currentLang . '_' . md5( json_encode( $args ) );
 	$cachedData    = get_transient( $cachingToken );
 	$cacheDuration = HOUR_IN_SECONDS;
 
