35e62c8cfc3f86caf987b9b40e5c76fab04e02f1
Tino Geißler <tinogeissler@proton.me>
2024-06-14 12:07:37 +0200

    <chore>(Filter Cache): add caching with pagination

--- web/app/themes/juniper-theme/blocks/filter/functions.php
+++ web/app/themes/juniper-theme/blocks/filter/functions.php
@@ -2,31 +2,34 @@
 
 namespace Blocks\Filter;
 
+require_once __DIR__ . '/prebuildCache/PrebuildCache.php';
+
+use blocks\filter\prebuildCache\PrebuildCache;
 use stdClass;
 use WP_Post;
 
 add_action(
 	'wp_enqueue_scripts', function () {
-	if ( has_block( 'acf/filter' ) ) {
-		$time       = time();
-		$theme_path = get_template_directory_uri();
-
-		wp_enqueue_style( 'filter-css', $theme_path . '/blocks/filter/style.css', [], $time, 'all' );
-		wp_enqueue_script( 'filter-js', $theme_path . '/blocks/filter/script.js', [], $time, true );
-
-		$attributes = [];
-		wp_enqueue_script( 'filterBlock', $theme_path . '/blocks/filter/build/frontend.js', [
-			'wp-blocks',
-			'wp-element',
-			'wp-editor',
-			'wp-api',
-			'wp-element',
-			'wp-i18n',
-			'wp-polyfill',
-			'wp-api-fetch',
-		],                 $time, true );
-		wp_localize_script( 'filterBlock', 'filterData', $attributes );
+	if ( ! has_block( 'acf/filter' ) ) {
+		return;
 	}
+
+	$time       = time();
+	$theme_path = get_template_directory_uri();
+
+	wp_enqueue_style( 'filter-css', $theme_path . '/blocks/filter/style.css', [], $time, 'all' );
+	wp_enqueue_script( 'filter-js', $theme_path . '/blocks/filter/script.js', [], $time, true );
+
+	wp_enqueue_script( 'filterBlock', $theme_path . '/blocks/filter/build/frontend.js', [
+		'wp-blocks',
+		'wp-element',
+		'wp-editor',
+		'wp-api',
+		'wp-element',
+		'wp-i18n',
+		'wp-polyfill',
+		'wp-api-fetch',
+	],                 $time, true );
 }
 );
 
@@ -42,6 +45,7 @@ add_filter( 'timber/acf-gutenberg-blocks-data/filter', function ( $context ) {
 		$context['fields']['filter_options'][ $key ]['label'] = $tax->label;
 		$context['fields']['filter_options'][ $key ]['name']  = $tax->name;
 
+		// INFO: when removing 'hide_empty' as a argument, then the return turns into an object
 		$terms = get_terms( [
 			                    'taxonomy'   => $option['filter_choices'],
 			                    'hide_empty' => false, // Set to true if you want to exclude terms with no posts.
@@ -96,35 +100,15 @@ function acf_load_taxonomy_field_choices( $field ) {
 	return $field;
 }
 
-add_filter( 'acf/load_field/name=filter_choices', 'Blocks\Filter\acf_load_taxonomy_field_choices' );
-//
-//add_action( 'rest_api_init', function () {
-//	register_rest_route( 'wps/v1', '/data', [
-//		'methods'             => 'GET',
-//		'callback'            => 'wps_filter_callback',
-//		'permission_callback' => '__return_true',
-//	] );}
-//);
+add_action( 'wp_ajax_nopriv_getPostFilter', 'Blocks\Filter\wps_filter_callback' );
+add_action( 'wp_ajax_getPostFilter', 'Blocks\Filter\wps_filter_callback' );
+function wps_filter_callback(): void {
+	$page      = $_GET['page'] ? intval( $_GET['page'] ) : 1;
+	$post_type = $_GET['post_type'] ?: '';
 
-function wps_filter_callback() {
-	$rawTaxonomies = ! empty( $_GET['taxonomies'] ) ? $_GET['taxonomies'] : "[]";
+	$post_data = wps_get_filter_posts( $post_type, $page );
 
-	// URL decode the parameter
-	$decodedTaxonomies = urldecode( $rawTaxonomies );
-
-	// Remove extra slashes
-	$decodedTaxonomies = stripslashes( $decodedTaxonomies );
-	// Decode the JSON string
-	$decodedTaxonomies = json_decode( $decodedTaxonomies );
-
-	$page      = ! empty( $_GET['page'] ) ? intval( $_GET['page'] ) : 1;
-	$post_type = ! empty( $_GET['post_type'] ) ? $_GET['post_type'] : '';
-	$search    = ! empty( $_GET['search'] ) ? $_GET['search'] : '';
-
-	$posts = wps_get_filter_posts( $post_type, $page );
-
-	return [];
-//	return $posts;
+	wp_send_json_success( $post_data, 200 );
 }
 
 function wps_get_filter_post_ids( $post_type ): array {
@@ -153,8 +137,6 @@ function wps_get_filter_post_ids( $post_type ): array {
 
 	$query = $wpdb->prepare($query, $replacements);
 
-	error_log($query);
-
 	return $wpdb->get_col($query);
 }
 
@@ -171,7 +153,7 @@ function table_exists( $table_name ): bool {
 	return in_array( $wpdb->prefix . $table_name, $existing_tables );
 }
 
-function wps_get_filter_posts( $post_type, $page ) {
+function wps_get_filter_posts( $post_type, $page = 0, $per_page = 6 ): array {
 
 	$currentLang   = apply_filters( 'wpml_current_language', null );
 	$cachingToken  = 'wps_filter_cache_' . $currentLang;
@@ -183,10 +165,10 @@ function wps_get_filter_posts( $post_type, $page ) {
 		$cachedData = null;
 	}
 
-	if ( empty($cachedData) ) {
-		$product_ids = wps_get_filter_post_ids($post_type );
+	if ( empty( $cachedData ) ) {
+		$product_ids = wps_get_filter_post_ids( $post_type );
 
-		if ( !empty($product_ids) ) {
+		if ( ! empty( $product_ids ) ) {
 			// set transient for 1 hour
 
 			set_transient( $cachingToken, $product_ids, $cacheDuration );
@@ -196,15 +178,11 @@ function wps_get_filter_posts( $post_type, $page ) {
 		$product_ids = $cachedData;
 	}
 
-	$posts = array_map('get_post', $product_ids);
+	//	$products_to_load = array_splice( $product_ids, $page * $per_page, $per_page);
 
-	$posts = array_filter($posts);
+	$post_arr = array_map( 'Blocks\Filter\map_post_to_filter_post_obj', $product_ids );
 
-	$post_arr = array_map('Blocks\Filter\map_post_to_filter_post_obj', $posts);
-
-	$data_arr['posts']       = $post_arr;
-
-	return $data_arr;
+	return array_filter( $post_arr );
 }
 
 /**
@@ -212,57 +190,12 @@ function wps_get_filter_posts( $post_type, $page ) {
  *
  * @return stdClass
  */
-function map_post_to_filter_post_obj( WP_Post $post ): stdClass {
-	$fields = get_fields( $post->ID );
-
-	$post_obj     = new stdClass();
-	$post_obj->ID = $post->ID;
-	//		$post_obj->fields               = get_fields(json_encode($post));
-	$post_obj->excerpt           = htmlspecialchars( wp_trim_excerpt( '', $post ) );
-	$post_obj->post_title        = htmlspecialchars( $post->post_title );
-	$post_obj->post_name         = htmlspecialchars( $post->post_name );
-	$post_obj->date              = htmlspecialchars( $post->post_date );
-	$post_obj->featured_image    = htmlspecialchars( get_the_post_thumbnail_url( $post ) );
-	$post_obj->link              = htmlspecialchars( get_permalink( $post ) );
-	$post_obj->price             = (int) ( wc_get_product( $post->ID ) )->get_regular_price();
-	$post_obj->subheadline       = htmlspecialchars( $fields['wps_sp_subheadline'] ?? '' );
-	$post_obj->description_title = htmlspecialchars( $fields['wps_sp_description_title'] ?? '' );
-	$post_obj->description_text  = htmlspecialchars( $fields['wps_sp_description_text'] ?? '' );
-	//		$post_obj->features_text        = htmlspecialchars( $fields['wps_sp_features_text'] ?? '' );
-	//		$post_obj->areas_of_application = htmlspecialchars( $fields['wps_sp_areas_of_application_text'] ?? '' );
-
-	$taxonomies = get_post_taxonomies( $post );
-
-	$taxonomy_data = [];
-	foreach ( $taxonomies as $taxonomy ) {
-		$terms = get_the_terms( $post, $taxonomy );
-		if ( $terms && ! is_wp_error( $terms ) ) {
-			foreach ( $terms as $term ) {
-				$taxonomy_data[ $taxonomy ][] = [
-					'term_id' => $term->term_id,
-					'name'    => $term->name,
-					'slug'    => $term->slug
-				];
-			}
-		}
+function map_post_to_filter_post_obj( $post_id, $reload_cache = false ): stdClass {
+	try {
+		return PrebuildCache::get_instance()->get_prebuild( $post_id, $reload_cache );
+	} catch ( \Exception $e ) {
+		return json_decode( PrebuildCache::get_instance()->generate_prebuild_json( $post_id ) );
 	}
-
-	$post_obj->taxonomies = $taxonomy_data;
-
-	$terms        = get_the_terms( $post, 'product_type' );
-	$product_type = $terms && ! is_wp_error( $terms )
-		? $terms[0]->name
-		: '';
-
-	$post_obj->product_type = $product_type;
-
-	$rendered_card = do_shortcode( "[wps_get_product_card product_id='{$post->ID}' encoding='ISO-8859-1']" );
-
-	$encoded_html = base64_encode( $rendered_card );
-
-	$post_obj->html = $encoded_html;
-
-	return $post_obj;
 }
 
 function display_woocommerce_notices_on_add_to_cart( $content ) {
